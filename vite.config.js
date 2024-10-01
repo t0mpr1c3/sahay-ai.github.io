63c56cb7689b8d0ed3e16ef450a8f214dd90a37a
Tejas <tejas.dce@gmail.com>
2024-09-30 23:50:21 -0400

    Deploy React Vite app to gh

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+  plugins: [react()],
+  base: '/sahay-ai.github.io/', // Replace with your GitHub repository name
 });
